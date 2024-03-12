70a55deb6379f9be09e2b12b40819e2150106e72
def1s <temnay2@icloud.com>
2024-03-12 21:09:01 +0300

    filter code changed

--- src/features/Filters/model/selectors/getFilteredGroups.ts
+++ src/features/Filters/model/selectors/getFilteredGroups.ts
@@ -1,5 +1,6 @@
 import { createSelector } from '@reduxjs/toolkit';
 import { StateSchema } from 'app/providers/StoreProvider';
+import { Group } from 'shared/api/fetchGroups';
 
 
 export const getFilteredGroups = createSelector(
@@ -7,26 +8,13 @@ export const getFilteredGroups = createSelector(
 	(state: StateSchema) => state.filters.currentFilters,
 	(groups, currentFilters) => {
 		const { closed, avatarColor, hasFriends } = currentFilters;
-		return groups.filter(group => {
-			let passFilter = true;
+		// каждый фильтр - это функция, которая возвращает true или false, можно легко добавить новые фильтры или убрать старые
+		const filters = [
+			(group: Group) => closed === null || group.closed === closed,
+			(group: Group) => avatarColor === null || group.avatar_color === avatarColor || avatarColor === 'any',
+			(group: Group) => hasFriends === null || (hasFriends ? group.friends && group.friends.length > 0 : !group.friends)
+		];
 
-			if (closed !== null) {
-				passFilter = group.closed === closed;
-			}
-
-			if (avatarColor !== null) {
-				passFilter = passFilter && (group.avatar_color === avatarColor || avatarColor === 'any');
-			}
-
-			if (hasFriends !== null) {
-				if (hasFriends) {
-					passFilter = passFilter && (group.friends && group.friends.length > 0);
-				} else {
-					passFilter = passFilter && !group.friends;
-				}
-			}
-
-			return passFilter;
-		});
+		return groups.filter(group => filters.every(filter => filter(group)));
 	}
 );
